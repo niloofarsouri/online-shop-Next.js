011795b639fa93ef6115875ec9e7daf83af54e84
niloofar souri <nlfrsouri@gmail.com>
2024-11-20 18:07:08 +0330

    edit components

--- src/pages/About-us/index.js
+++ src/pages/About-us/index.js
@@ -1,4 +1,6 @@
 import style from '@/pages/About-us/index.module.css'
+import me from '@/pages/'
+import Image from 'next/image';
 
 
 
@@ -10,7 +12,12 @@ function About() {
             <section className={style.container}>
                 <div className={style.main}>
                     <div className={style.main_image}>
-                        <img src="./img/IMG_6917.jpg" />
+                        {/* <img src="./img/IMG_6917.jpg" /> */}
+                        <Image
+                            src={'/img/IMG_6917.JPG'}
+                            width={100}
+                            height={400}
+                        />
                     </div>
 
                     <h1>Niloofar Souri</h1>
